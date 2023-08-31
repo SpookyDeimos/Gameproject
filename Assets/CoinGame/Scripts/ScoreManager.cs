c975b2ef2ea9b2f98d8aa6531893279238d5f4f9
SpookyDeimos <angelcecytea666@gmail.com>
2023-08-31 15:46:22 -0600

    Update ScoreManager.cs
    Added XML documentation to ScoreManager script in coin game

--- Assets/CoinGame/Scripts/ScoreManager.cs
+++ Assets/CoinGame/Scripts/ScoreManager.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
 using TMPro;
 
+/// <summary>
+/// Manages the game's score and updates the score UI.
+/// </summary>
 public class ScoreManager : MonoBehaviour
 {
-    public TextMeshProUGUI scoreText; 
+    /// <summary>
+    /// Reference to the TextMeshPro Text element in the UI for displaying the score.
+    /// </summary>
+    public TextMeshProUGUI scoreText;
+
     private int score = 0;
 
     void Start()
@@ -11,17 +18,20 @@ public class ScoreManager : MonoBehaviour
         UpdateScoreText();
     }
 
-    
+    /// <summary>
+    /// Updates the score by adding the given number of points and refreshes the UI.
+    /// </summary>
     public void UpdateScore(int points)
     {
         score += points;
         UpdateScoreText();
     }
 
-    
+    /// <summary>
+    /// Updates the Text element with the current score.
+    /// </summary>
     private void UpdateScoreText()
     {
         scoreText.text = "Coins: " + score.ToString();
     }
 }
-
