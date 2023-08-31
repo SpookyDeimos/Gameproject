1a01aba8d6910cde4c9318a4ead838cafbeb5cea
SpookyDeimos <angelcecytea666@gmail.com>
2023-08-31 15:28:40 -0600

    XML documentation StartPrayingAnimation
    Added XML documentation to StartPrayingAnimation

--- Assets/AnimationScene/Script/StartPrayingAnimation.cs
+++ Assets/AnimationScene/Script/StartPrayingAnimation.cs
@@ -1,5 +1,9 @@
 using UnityEngine;
 
+
+/// <summary>
+/// This class handles starting various animations using different keyboard inputs
+/// </summary>
 public class StartPrayingAnimation : MonoBehaviour
 {
     private Animator animator;
@@ -9,20 +13,23 @@ public class StartPrayingAnimation : MonoBehaviour
         animator = GetComponent<Animator>();
     }
 
+    /// <summary>
+    /// Checks for input and triggers playing different animations depending on what key is pressed
+    /// </summary>
     void Update()
     {
-        
+        /// triggers praying animation when P is pressed
         if (Input.GetKeyDown(KeyCode.P))
         {
             
             animator.SetTrigger("StartPrayingAnimation");
         }
-
+         /// triggers spin animation when S is pressed
      if (Input.GetKeyDown(KeyCode.S))
         {
             animator.SetTrigger("SpinTrigger");
         }
-
+         /// triggers kick animation when K is pressed
      if (Input.GetKeyDown(KeyCode.K))
         {
             animator.SetTrigger("KickTrigger");
